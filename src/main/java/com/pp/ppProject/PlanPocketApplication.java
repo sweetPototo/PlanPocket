d6d12a84f876c07a888680a94093d8284d77ee4c
“sweetPototo” <sweetpototo11@gmail.com>
2024-08-02 20:07:02 +0900

    first commit

---
+++ src/main/java/com/pp/ppProject/PlanPocketApplication.java
@@ -0,0 +1,13 @@
+package com.pp.ppProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanPocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanPocketApplication.class, args);
+	}
+
+}
