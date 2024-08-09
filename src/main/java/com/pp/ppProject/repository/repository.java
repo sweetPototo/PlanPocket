7fa0a2c664df86351c30dee3c1c6ce634ba33d5d
changyeonHeo <qnzlckddus@naver.com>
2024-08-09 15:55:51 +0900

    lombok 설정전2

--- src/main/java/com/pp/ppProject/repository/repository.java
+++ src/main/java/com/pp/ppProject/repository/repository.java
@@ -6,16 +6,19 @@ import org.springframework.stereotype.Repository;
 import com.pp.ppProject.domain.MemberEntity;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 import lombok.RequiredArgsConstructor;
 
 @Repository
 @RequiredArgsConstructor
 public class repository {
 
-	private final EntityManager em;
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+    EntityManager em = emf.createEntityManager(); 
 	
 	public MemberEntity save(MemberEntity m) {
-		em.persist(m);
+		em.persist("abcd");
 		return m;
 	}
 }
