b0551b5a7adac31e472fc9d7f99d4f17d82367fa
changyeonHeo <qnzlckddus@naver.com>
2024-08-09 22:47:06 +0900

    DB member생성성공

--- src/main/java/com/pp/ppProject/domain/MemberEntity.java
+++ src/main/java/com/pp/ppProject/domain/MemberEntity.java
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-package com.pp.ppProject.domain;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-@Entity
-public class MemberEntity {
-	@Id 
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int memberNum;
-	private String memberId;
-	private String memberPw;
-	private String memberName;
-=======
 //domain
 package com.pp.ppProject.domain;
 
@@ -25,21 +8,14 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(name = "member")
 public class MemberEntity {
 
-	@Id // 기본키 할당
-	@GeneratedValue
-	private int id;
+   @Id // 기본키 할당
+   @GeneratedValue
+   private int id;
+   private String name;
+   private String homeAddress;
+   private String workAddress;
 
-	private String name;
-
-	
-	
-	private String homeAddress;
-
-	
-	
-	private String workAddress;
-
->>>>>>> 7fa0a2c664df86351c30dee3c1c6ce634ba33d5d
 }
