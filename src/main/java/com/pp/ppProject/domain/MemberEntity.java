6b6a0f2b2df60a682ae8ca4c2de1adde4205cf33
MOCHA\ehihit <leebh903@naver.com>
2024-08-09 22:30:34 +0900

    lombok, JPA 설정 완료

--- src/main/java/com/pp/ppProject/domain/MemberEntity.java
+++ src/main/java/com/pp/ppProject/domain/MemberEntity.java
@@ -1,16 +1,20 @@
 package com.pp.ppProject.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@Table(name = "member")
 public class MemberEntity {
-	@Id 
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int memberNum;
-	private String memberId;
-	private String memberPw;
-	private String memberName;
+
+   @Id // 기본키 할당
+   @GeneratedValue
+   private int id;
+   private String name;
+   private String homeAddress;
+   private String workAddress;
+
 }
