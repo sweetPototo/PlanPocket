b895d9a60576647c2e8d0684e16a9e6899d0044b
MOCHA\ehihit <leebh903@naver.com>
2024-08-12 19:04:39 +0900

    annotation add, ScrapEntity merge

--- src/main/java/com/pp/ppProject/domain/MemberEntity.java
+++ src/main/java/com/pp/ppProject/domain/MemberEntity.java
@@ -5,8 +5,11 @@ import java.sql.Date;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
@@ -18,6 +21,8 @@ import lombok.Getter;
     initialValue = 1, // 시퀀스 시작 값
     allocationSize = 1 // 증가 값
 )
+@AllArgsConstructor //필드의 모든 값을 매개변수로 받는 생성자 생성
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //기본 생성자 생성, 같은 패키지나 상송받은 클래스만 생성자 접근
 
 public class MemberEntity {
 
--- src/main/java/com/pp/ppProject/domain/ScheduleEntity.java
+++ src/main/java/com/pp/ppProject/domain/ScheduleEntity.java
@@ -6,27 +6,31 @@ import java.sql.Date;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Entity
-@Getter
-@Builder
-@SequenceGenerator(
-	    name = "SCHEDULE_SEQ_GENERATOR", // 사용할 시퀀스 이름
+@Entity // Entity임을 명시, Entity명 schedule
+@Getter // getter 메소드 생성
+@Builder // Builder 사용
+@SequenceGenerator( // sequence 생성
+	    name = "SCHEDULE_SEQ_GENERATOR", // 사용할 sequence 이름
 	    sequenceName = "schedule_seq",
 	    initialValue = 1,
 	    allocationSize = 1 
 	)
-@Table(name = "schedule")
+@Table(name = "schedule") // table명을 schedule 로 지정
+@AllArgsConstructor // 필드의 모든 값을 매개변수로 받는 생성자 생성
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자 생성, 같은 패키지나 상송받은 클래스만 생성자 접근
 public class ScheduleEntity {
 	
-	@Id
+	@Id // 기본키 할당
 	@GeneratedValue( strategy = GenerationType.SEQUENCE, generator = "SCHEDULE_SEQ_GENERATOR" )
 	private int scheNo;
 	
-	@Column(length = 50)
-	private String memberId;
+	private int memberNo ;
 	
 	@Column(length = 2000)
 	private String scheContent;
