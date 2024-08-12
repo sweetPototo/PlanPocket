75c9705b7c5b146319369aa4b5e10e5027bae093
sweetpototo <sweetPototo11@gmail.com>
2024-08-12 17:20:00 +0900

    complete Entity

---
+++ src/main/java/com/pp/ppProject/domain/AcountEntity.java
@@ -0,0 +1,33 @@
+package com.pp.ppProject.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity //Entity임을 명시, Entity명 account
+@Table(name = "account")  //table명을 acount로 지정
+@Getter  //getter 메소드 생성
+@SequenceGenerator(  //시퀀스 생성기 설
+		name = "ACCOUNT_SEQ_GENERATOR",
+		sequenceName = "ACCOUNT_SEQ"
+)
+@Builder  //Builder 사용
+@AllArgsConstructor //필드의 모든 값을 매개변수로 받는 생성자 생성
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //기본 생성자 생성, 같은 패키지나 상송받은 클래스만 생성자 접근
+public class AcountEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ACCOUNT_SEQ_GENERATOR")
+	private int accountNo;
+	private String accountName;
+	private int accountCategory;
+	private int memberNo;
+}
---
+++ src/main/java/com/pp/ppProject/domain/TransactionBKEntity.java
@@ -0,0 +1,34 @@
+package com.pp.ppProject.domain;
+
+import java.sql.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity //Entity임을 명시, Entity명 account
+@Table(name = "transactionBK")  //table명을 acount로 지정
+@Getter  //getter 메소드 생성
+@Builder  //Builder 사용
+@AllArgsConstructor //필드의 모든 값을 매개변수로 받는 생성자 생성
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //기본 생성자 생성, 같은 패키지나 상송받은 클래스만 생성자 접근
+public class TransactionBKEntity {
+
+	private int tranNoBk;
+	private int memberNoBk;
+	private int accountNoBk;
+	private int tranTypeBk;
+	private int tranAmountBk;
+	private int tranBalanceBk;
+	@DateTimeFormat(pattern = "yy/MM/dd")
+	private Date tranDateBk;
+	private int tranCategoryBk;
+	private int tranDetailBk;
+}
