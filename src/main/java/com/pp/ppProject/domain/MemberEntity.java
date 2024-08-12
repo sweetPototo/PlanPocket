bbe750039577472a1f0c2202c7bab3432bc648bf
changyeonHeo <qnzlckddus@naver.com>
2024-08-12 16:16:44 +0900

    member entity 생성

--- src/main/java/com/pp/ppProject/domain/MemberEntity.java
+++ src/main/java/com/pp/ppProject/domain/MemberEntity.java
@@ -1,21 +1,50 @@
 //domain
 package com.pp.ppProject.domain;
 
+import java.sql.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+
 
 @Entity
 @Getter
-@Setter
+@Builder
 @Table(name = "member")
 public class MemberEntity {
 
    @Id // 기본키 할당
-   @GeneratedValue
-   private int id;
-   private String name;
-   private String homeAddress;
-   private String workAddress;
-
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private int memberNo;
+   
+   @Column(length = 50)
+   private String memberId;
+   
+   @Column(length = 50)
+   private String memberPasswd;
+   
+   @Column(length = 50)
+   private String memberName;
+   
+   @Column(length = 11)
+   private String memberTel;
+   
+   @Column(length = 50)
+   private String memberEmail;
+   
+   @Column(length = 100)
+   private String memberImage;
+   
+   @Column(length = 2000)
+   private String memberIntro;
+   
+   @DateTimeFormat(pattern = "yy/MM/dd")
+   private Date memberJoinDate;
+   
+   @DateTimeFormat(pattern = "yy/MM/dd")
+   private Date memberOutDate;
+  
 }
